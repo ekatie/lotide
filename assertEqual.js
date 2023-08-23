c70a1c943037ed7c4668c09e82f2b4589939b823
Katie K <katiekucsera@gmail.com>
2023-08-23 14:40:03 -0400

    Updated code with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual,expected) {
   if (actual === expected) {
-    console.log("✅👍😁🥳 Assertion Passed: " + [actual] + " === " + [expected]);
+    console.log(`✅👍😁🥳 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("❌👎😢💩 Assertion Failed: " + [actual] + " !== " + [expected]);
+    console.log(`❌👎😢💩 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
