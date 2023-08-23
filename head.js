725a8623f219c0af1d0748120c8c9fa17f3edabe
Katie K <katiekucsera@gmail.com>
2023-08-23 14:49:51 -0400

    Created a function that prints out the first value in an array and tests for expected result

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅👍😁🥳 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌👎😢💩 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  if (array === "") {
+    return undefined;
+  } else {
+    return array[0];
+  }
+};
+
+assertEqual(head([5,6,7]),5);
+assertEqual(head(["Hello","Lighthouse","Labs"]),"Hello");
+assertEqual(head([2]),5);
