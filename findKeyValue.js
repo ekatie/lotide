19913f50eccf4224a1c9659fe6aaba21dccb82b4
Katie K <katiekucsera@gmail.com>
2023-08-30 12:51:45 -0400

    Add comments and improve organization and readability of code

--- findKeyValue.js
+++ findKeyValue.js
@@ -6,6 +6,13 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+/**
+ * This function takes in an object and a value, then searches the object for the key of the indicated value.
+ * @param {object} inputObject 
+ * @param {primitive value} value 
+ * @returns The key of the value as it occurs in the object.
+ */
+
 const findKeyByValue = function(inputObject, value) {
   for (let key in inputObject) {
     if (inputObject[key] === value) {
@@ -15,11 +22,11 @@ const findKeyByValue = function(inputObject, value) {
 };
 
 // test code
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire"
-};
+// const bestTVShowsByGenre = {
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama: "The Wire"
+// };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
