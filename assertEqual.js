e835615ab036e8e6a347a90d86b8a50f5ce0e782
Katie K <katiekucsera@gmail.com>
2023-08-30 12:51:01 -0400

    Add comments and improve organization and readability of code

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,10 @@
-const assertEqual = function(actual,expected) {
+/**
+ * This function compares the actual output and expected output of other functions to test whether they are functioning as intended. It print the result to the console.
+ * @param {primitive value} actual 
+ * @param {primitive value} expected 
+ */
+
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅👍😁🥳 Assertion Passed: ${actual} === ${expected}`);
   } else {
