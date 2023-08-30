d6f6d4040f60978a79b5b02fd0efc6097d25d76d
Katie K <katiekucsera@gmail.com>
2023-08-30 12:31:44 -0400

    Add comments and improved oragnization and readability of code

--- letterPositions.js
+++ letterPositions.js
@@ -17,15 +17,22 @@ const assertArraysEqual = function(inputArrayOne, inputArrayTwo) {
   }
 };
 
+/**
+ * This function will return all the indices (zero-based positions) in the string where each character is found.
+ * @param {string} sentence takes in a string to count the position of letters
+ * @returns object containing the letters of the string and the location of each letter at their respective indices.
+ */
 const letterPositions = function(sentence) {
   const results = {};
 
-  for (let i = 0; i < sentence.length; i++) {
-    if (sentence[i] !== ' ') {
-      if (!results[sentence[i]]) {
-        results[sentence[i]] = [i];
-      } else if (results[sentence[i]]) {
-        results[sentence[i]].push(i);
+  for (let index = 0; index < sentence.length; index++) {
+    const char = sentence[index];
+
+    if (char !== " ") {
+      if (!results[char]) {
+        results[char] = [index];
+      } else if (results[char]) {
+        results[char].push(index);
       }
     }
   }
