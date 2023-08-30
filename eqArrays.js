8b9a4b82ab316bcc0c309f69fb373d5742cd0205
Katie K <katiekucsera@gmail.com>
2023-08-30 15:35:44 -0400

    Add comment to explain function of code

--- eqArrays.js
+++ eqArrays.js
@@ -23,9 +23,3 @@ const eqArrays = function(arrayOne, arrayTwo) {
   }
   return true;
 };
-
-// test cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
