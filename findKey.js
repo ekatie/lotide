f3974f8ebdf29639957c71cf367821f0abd4ec78
Katie K <katiekucsera@gmail.com>
2023-09-01 15:14:20 -0400

    Refactore code and remove test code

--- findKey.js
+++ findKey.js
@@ -1,17 +1,14 @@
 /**
  * This function takes in an object and a callback function and returns the key of the value that matches the callback function condition.
- * @param {object} object - Input object to search
- * @param {callback function} callback - Condition for value to search object for.
+ * @param {object} inputObject - Input object to search
+ * @param {callback function} condition - Function to calculate condition for value to search object for.
  * @returns - String key of found value.
  */
 
-const findKey = function(object, callback) {
-
-  let objectKeys = Object.keys(object);
-
-  for (let i = 0; i < objectKeys.length; i++) {
-    if (callback(object[objectKeys[i]])) {
-      return objectKeys[i];
+const findKey = function(inputObject, condition) {
+  for (let key in inputObject) {
+    if (condition(inputObject[key])) {
+      return key;
     }
   }
 };
