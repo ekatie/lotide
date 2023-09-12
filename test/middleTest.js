ced6cd69662060c7f120a726216243dbd9cd9cf4
Katie K <katiekucsera@gmail.com>
2023-09-12 17:03:05 -0400

    Update test code to use Mocha and Chai for testing and assertions

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,22 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+describe("#middle", () => {
+
+  it("returns [2,3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns [5] for [1, 3, 5, 7, 9]", () => {
+    assert.deepEqual(middle([1, 3, 5, 7, 9]), [5]);
+  });
+
+  it("returns ['my', 'name'] for ['hello', 'there', 'my', 'name', 'is', 'katie']", () => {
+    assert.deepEqual(middle(['hello', 'there', 'my', 'name', 'is', 'katie']), ['my', 'name']);
+  });
+
+  it("returns ['name'] for ['hello', 'my', 'name', 'is', 'katie']", () => {
+    assert.deepEqual(middle(['hello', 'my', 'name', 'is', 'katie']), ['name']);
+  });
+
+});
