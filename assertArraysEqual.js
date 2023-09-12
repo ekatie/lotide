2cdde8b97b2b8d1661c1b295f53f787c74931c9a
Katie K <katiekucsera@gmail.com>
2023-09-12 16:06:35 -0400

    Refactor code by separating out test cases and exporting the function to the test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,11 @@
+const eqArrays = require('./eqArrays');
+
 /**
  * This function takes in two arrays, compares their values and prints to the console to indicate whether they match or not.
- * @param {array} arrayOne - First array to compare
- * @param {array} arrayTwo - Second array to compare
- * @returns Results of comparison - whether the arrays match or not.
- */
-
-const eqArrays = function(arrayOne, arrayTwo) {
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    } else {
-      continue;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(inputArrayOne, inputArrayTwo) {
+ * @param {array} inputArrayOne - First array to compare
+ * @param {array} inputArrayTwo - Second array to compare
+ * Print out results of comparison - whether the arrays match or not.
+ */const assertArraysEqual = function(inputArrayOne, inputArrayTwo) {
   if (eqArrays(inputArrayOne, inputArrayTwo) === false) {
     console.log("The input arrays do not match.");
   } else if (eqArrays(inputArrayOne, inputArrayTwo) === true) {
@@ -24,9 +13,4 @@ const assertArraysEqual = function(inputArrayOne, inputArrayTwo) {
   }
 };
 
-// test arrays
-// let testArray1 = [1,"hi","5",7];
-// let testArray2 = [1,"hi","5",7];
-
-// // call function
-// assertEqualArrays(testArray1,testArray2);
+module.exports = assertArraysEqual;
