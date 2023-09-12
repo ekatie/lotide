672551e48820b417e62337b49a2f8d1c9af32b90
Katie K <katiekucsera@gmail.com>
2023-09-12 15:52:25 -0400

    Refactor code by separating out test cases and exporting the function to the test file

--- tail.js
+++ tail.js
@@ -1,23 +1,12 @@
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`✅👍😁🥳 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌👎😢💩 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+/**
+ * This function removes the first index (head) of the input array and returns the rest.
+ * @param {array} array - Input array.
+ * @returns - The tail end of the array (all but the first index value).
+ */
 
 const tail = function(array) {
   let arrayTail = array.slice(1);
   return arrayTail;
 };
 
-// const result = tail(["Hello","Lighthouse","Labs"]);
-// assertEqual(result.length,2);
-// assertEqual(result[0],"Lighthouse");
-// assertEqual(result[1],"Labs");
-
-// const words = ["Yo Yo","Lighthouse","Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length,3); // original array should still have 3 elements!
-
-// console.log(tail([]));
+module.exports = tail;
