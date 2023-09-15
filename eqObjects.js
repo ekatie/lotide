a225170e45bace5152cbefbe88c1bcdb2afe3cda
Katie K <katiekucsera@gmail.com>
2023-09-15 14:37:07 -0400

    Refactor code to include recursion and be able to loop through endless nested objects for comparison

--- eqObjects.js
+++ eqObjects.js
@@ -10,23 +10,25 @@ const eqArrays = require("./eqArrays");
 const eqObjects = function(object1, object2) {
 
   // Check if both objects have the same number of keys
-  if (Object.keys(object1).length === Object.keys(object2).length) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  // Compare key values for each object, both for arrays and not arrays
+  for (const key in object1) {
 
-    // Compare key values for each object, both for arrays and not arrays
-    for (const key in object1) {
-
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      }
-      else if (object1[key] !== object2[key]) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
+    } else if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
     }
-    return true;
   };
-  return false;
+  return true;
 };
 
 module.exports = eqObjects;
