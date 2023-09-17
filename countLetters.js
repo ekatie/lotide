3b433910ff47e1f83e58d726ecf21beaaf227dfc
Katie K <katiekucsera@gmail.com>
2023-09-17 14:30:19 -0400

    Remove test code and move to separate file

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 /**
  * This function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
  * @param {array} string - Takes in a string to count
@@ -23,27 +21,3 @@ const countLetters = function(string) {
 };
 
 module.exports = countLetters;
-
-// console.log(countLetters("lighthouse in the house"));
-
-// test code
-// let string = "lighthouse in the house";
-// let expected = {
-//   l: 1,
-//   i: 2,
-//   g: 1,
-//   h: 4,
-//   t: 2,
-//   o: 2,
-//   u: 2,
-//   s: 2,
-//   e: 3,
-//   n: 1,
-// };
-
-// let letterCount = countLetters(string, {'l': 1, 'i': 2, 'h': 4, 'z': undefined});
-
-// assertEqual(letterCounts['l'], 1);
-// assertEqual(letterCounts['i'], 2);
-// assertEqual(letterCounts['h'], 4);
-// assertEqual(letterCounts['z'], undefined);
